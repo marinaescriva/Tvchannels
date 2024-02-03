84399418e02e680a8fce246739b900479455a829
marinaescriva <marinaescriva.24@gamil.com>
2024-02-03 13:27:17 +0100

    adding pilot button on tv

--- js/main.js
+++ js/main.js
@@ -1,33 +1,69 @@
 
-const buttons = document.getElementsByClassName("button")
+// const buttons = document.getElementsByClassName("button")
 
-let arrayButtons = Array.from(buttons)
+// let arrayButtons = Array.from(buttons)
 
-let screen = document.getElementById("screen")
+// let screen = document.getElementById("screen")
 
+// let power = false;
+
+// document.getElementById("piloto").addEventListener("click", (evento) => {
+//     if (power) {
+//         screen.classList.remove("pilotoOff");
+//         screen.classList.add("pilotoOn");       
+//     }
+// })
 let power = false;
 
-document.getElementById("onOff").addEventListener(("click",()) =>{
-
-    screen.classList.remove(screenClassList)
-    screen.classList.add("buttonPower" + ())
-})
-
+document.getElementById('buttonPower').addEventListener('click', () => {
     power = !power;
+    if (power) {
+        getElementById("piloto").classList.remove("pilotoOff");
+        getElementById("piloto").classList.add("pilotoOn");
+    } else {
+        getElementById("piloto").classList.remove("pilotoOn");
+        getElementById("piloto").classList.add("pilotoOff");
+    }
+    // console.log(power);
+});
+
+
+
+// function change(){
+
+//     let apagado = document.getElementById("divUno");
+//     let encendido = document.getElementsByClassName("divDos");
+
+//     if(apagado.hidden == true && encendido[0].hidden == false);{
+//         apagado.hidden = false;
+//         encendido[0].hidden = true;
+//         } else{
+//         apagado.hidden = true;
+//         encendido[0].hidden = false;
+//     }
+// }
+
+// document.getElementById("onOff").addEventListener(("click",()) =>{
+
+//     screen.classList.remove(screenClassList)
+//     screen.classList.add("buttonPower" + ())
+// })
+
+//     power = !power;
         
-    arrayButtons.map(
-        item => {
-            item.addEventListener("click", (evento) => {
+//     arrayButtons.map(
+//         item => {
+//             item.addEventListener("click", (evento) => {
                 
-                screen.classList.remove(screen.classList[screen.classList.length - 1])
-                screen.classList.add("canal" + evento.target.id.slice(-1))
-            })
-        }
-    )
+//                 screen.classList.remove(screen.classList[screen.classList.length - 1])
+//                 screen.classList.add("canal" + evento.target.id.slice(-1))
+//             })
+//         }
+//     )
 
             
         
 
  
-//probando el botón power
+// //probando el botón power
 
