a9d02c501d4b7947c3b7b6a8776b02c0f3aa9c69
marinaescriva <marinaescriva.24@gamil.com>
2024-02-04 13:34:18 +0100

    Tv is on and the channels are available but the powerbutton  off didnt return

--- js/main.js
+++ js/main.js
@@ -33,13 +33,18 @@
 //     console.log(power);
 // });
 
-let power = false;
-let pilot = document.getElementById("piloto");
-let onOrOff =document.getElementById("buttonPower");
-let screen = document.getElementById("screen");
+let power = false; //because the tv starts off.
+
+let pilot = document.getElementById("piloto"); // piloto selected
+let onOrOff =document.getElementById("buttonPower"); // buttonPower selected
+let screen = document.getElementById("screen"); // screen selected
+
+// let tvStatus = screen.classList.remove("channel" && e.target.id.slice(-1))
 
 onOrOff.addEventListener('click', () => {
+    
     power = !power; //after click on the tv is true, so its on now.
+    // tvStatus = true;
 
     console.log(power);
     if (power) {
@@ -47,84 +52,57 @@ onOrOff.addEventListener('click', () => {
         screen.classList.add("screenColor") //screen is available. and the class is now: "basicScreen"
         pilot.classList.remove("pilotoOff"); //if power is true the class pilotoOff is deleted 
         pilot.classList.add("pilotoOn"); //and the class pilotoOn is added.
-        console.log(pilot.classList);
     } else {
         screen.classList.remove("screenColor")
         screen.classList.add("screenBlack") //screen is available. and the class is now: "basicScreen"
         pilot.classList.remove("pilotoOn"); //but, to switch to off its necessary to delet the class on.
         pilot.classList.add("pilotoOff");
-        console.log(pilot.classList);
     }
 });
+//when the power=true means tv is on so, if its true the channels will be available.
 
+const buttons = document.getElementsByClassName("button") //all buttons are selected for...
+
+let arrayButtons = Array.from(buttons)
+
+arrayButtons.map(
+    item => {
+        item.addEventListener("click", (e) =>{
+
+            if (power==true){
+
+            // screen.classList.remove("screenColor")
+            // screen.classList.add("channel")
+            screen.classList.remove(screen.classList[screen.classList.length - 1])
+            screen.classList.add("channel" + e.target.id.slice(-1))
+
+            }
+
+        })
+    }
+)
+
+
+
+
+
+// 
 
 // while the buttonPower is true, the tv is on so you can press on the channels.
 
-// so, if pilotoOn is true, the tv is on - tvOn.
+        // const buttons = document.getElementsByClassName("button")
 
-// let tvOn = pilot.classList.value("pilotoOn");
+        // let arrayButtons = Array.from(buttons)
 
+        // screen = document.getElementById("screen")
 
-// if (power==true){
-
-//         const buttons = document.getElementsByClassName("button")
-
-//         let arrayButtons = Array.from(buttons)
-
-//         let screen = document.getElementById("screen")
-
-//         arrayButtons.map(
-//             item => {
-//                 item.addEventListener("click", (event) => {
-//                     screen.classList.remove(screen.classList[screen.classList.length - 1])
-//                     screen.classList.add("canal" + event.target.id.slice(-1))
-//                 })
-//             }
-//         )
-//         }
-
-
-
-
-
-
-
-
-// function change(){
-
-//     let apagado = document.getElementById("divUno");
-//     let encendido = document.getElementsByClassName("divDos");
-
-//     if(apagado.hidden == true && encendido[0].hidden == false);{
-//         apagado.hidden = false;
-//         encendido[0].hidden = true;
-//         } else{
-//         apagado.hidden = true;
-//         encendido[0].hidden = false;
-//     }
-// }
-
-// document.getElementById("onOff").addEventListener(("click",()) =>{
-
-//     screen.classList.remove(screenClassList)
-//     screen.classList.add("buttonPower" + ())
-// })
-
-//     power = !power;
-        
-//     arrayButtons.map(
-//         item => {
-//             item.addEventListener("click", (evento) => {
-                
-//                 screen.classList.remove(screen.classList[screen.classList.length - 1])
-//                 screen.classList.add("canal" + evento.target.id.slice(-1))
-//             })
-//         }
-//     )
-
-            
-        
-
- 
-// //probando el botón power
+        // arrayButtons.map(
+        //     item => {
+        //         item.addEventListener("click", (event) => {
+        //             screen.classList.remove(screen.classList[screen.classList.length - 1])
+        //             screen.classList.add("channel" + event.target.id.slice(-1))
+        //         })
+        //     }
+        // )
+        // }
 
